c7100a8328f77631f37f9b154db1dc6f20815bce
maybeags <suren0709@gmail.com>
2025-04-04 20:22:47 +0900

    feat: uploadeD

--- combined_project/frontend/carfront/src/api/carapi.ts
+++ combined_project/frontend/carfront/src/api/carapi.ts
@@ -2,7 +2,7 @@ import { CarResponse } from "../types";
 import axios from "axios";
 
 export const getCars = async (): Promise<CarResponse[]> => {
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`);
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`);
 
-    return response.data._embedded.cars;
-  }
+  return response.data._embedded.cars;
+}
