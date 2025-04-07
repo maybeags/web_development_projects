05b5d061224c86e43a844fb8730a5aa6ef28ac76
maybeags <suren0709@gmail.com>
2025-04-07 20:47:30 +0900

    feat: uploadeD

---
+++ 10000hoursrule/src/components/InputForm.jsx
@@ -0,0 +1,11 @@
+import { Box, TextField, Typography, Button } from "@mui/material";
+
+const InputForm = ({ field, setField, time, setTime, handleCalculate }) => {
+  return(
+    <Box>
+    
+    </Box>
+  );
+}
+
+export default InputForm;
