ff2027f62bcf3eecdf1733164f602a470a37ccce
maybeags <suren0709@gmail.com>
2025-04-08 20:43:38 +0900

    Feat: react-ize project finished

--- 10000hoursrule/src/components/MotivationalModal.jsx
+++ 10000hoursrule/src/components/MotivationalModal.jsx
@@ -5,7 +5,8 @@ import LicatImage from '../assets/licat.png';
 const MotivationalModal = ({ open, handleClose }) => {
 
   return(
-    <Box
+    <Modal open={open} onClose={handleClose}>
+      <Box
       sx={{
         position: 'absolute',
         top: '50%',
@@ -19,12 +20,31 @@ const MotivationalModal = ({ open, handleClose }) => {
         flexDirection: 'column',
         alignItems: 'center',
       }}
-    >
-      <Typography variant="h1" align="center" sx={{ fontFamily: }}>
-        파이팅!!! 📢
-      </Typography>
-
-    </Box>
+      >
+        <Typography variant="h1" align="center" sx={{ fontFamily: 'OTEnjoystoriesBA', color: '#5B2386', fontSize: '3rem' }}>
+          파이팅!!! 📢
+        </Typography>
+        <Typography variant="h5" align="center" sx={{ color: '#5B2386', mb: 3 }}>
+          당신의 꿈을 응원합니다!
+        </Typography>
+        <Box sx={{ textAlign: 'center', mb: 3 }}>
+          <img src={LicatImage} alt="사자고양이" />
+        </Box>
+        <Button
+          fullWidth
+          variant="contained"
+          sx={{
+            backgroundColor: '#FCEE21', color: '#5B2386', mb: 1
+          }}
+          onClick={handleClose}
+        >
+          종료하고 진짜 훈련하러 가기 🖥🖱
+        </Button>
+        <Typography variant="caption" display="block" align="center" color="gray">
+          (그냥 닫기 버튼입니다.)
+        </Typography>
+      </Box>
+    </Modal>
   );
 }
 
